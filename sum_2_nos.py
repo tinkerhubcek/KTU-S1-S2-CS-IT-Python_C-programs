630a2f05100222ba33e7b8bfc5ca2fe4ce342f00
Athul Cyriac Ajay <40897573+Athul-CA@users.noreply.github.com>
2018-10-10 21:59:27 +0530

    Add files via upload
    Program For Calculating Sum of Two Numbers
    Inputting One after One

---
+++ sum_2_nos.py
@@ -0,0 +1,8 @@
+#Input the first Number
+a=int(input("Enter the 1st Number: "))
+#Input The Second Number
+b=int(input("Enter the 2nd Number: "))
+#Arithematic
+sum=a+b
+#Printing
+print("The sum of ",a," and ",b," is ",sum)
